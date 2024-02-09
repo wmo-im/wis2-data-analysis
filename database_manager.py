9251babcdb7e7472b606c9285ee92e2575ac7973
celgarrai <chemseddine123@gmail.com>
2024-02-09 12:58:54 +0100

    Update database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -14,30 +14,37 @@ BUFR_KEYS_FILE = 'BUFRKeys.ini'
 class DatabaseManager:
     def __init__(self):
         # Initialize the database connection pool
-        db_config = ConfigReader.read_config(os.path.join(CONFIG_DIR, DATABASE_CONFIG_FILE), 'Database')
-        self.connection_pool = psycopg2.pool.SimpleConnectionPool(
-            1,
-            10000,
-            **db_config
-        )
+        # Initialize the database connection pool
+        database_config = ConfigReader.read_config(os.path.join(CONFIG_DIR, DATABASE_CONFIG_FILE), 'Database')
+        self.db_config = {
+            'dbname': database_config['dbname'],
+            'user': database_config['user'],
+            'password': database_config['password'],
+            'host': database_config['host'],
+            'port': database_config['port']
+        }
 
     def insert_message_data(self, data_list):
+        db_config = ConfigReader.read_config(os.path.join(CONFIG_DIR, DATABASE_CONFIG_FILE), 'Database')
         # Insert message data into the 'message' table
         query = """
             INSERT INTO message (topic, publication_timestamp, data_id, canonical_url, wigos_station_identifier)
             VALUES (%s, %s, %s, %s, %s)
             RETURNING id;
         """
-        connection = self.connection_pool.getconn()
-        try:
-            with connection, connection.cursor() as cursor:
-                for data in data_list:
-                    cursor.execute(query, (data['topic'], data['publication_timestamp'], data['data_id'], data['canonical_url'], data['wigos_station_identifier']))
-                    message_id = cursor.fetchone()[0]
-                    return message_id
-        finally:
-            self.connection_pool.putconn(connection)
-
+        
+        # Create a new connection for each call
+        with psycopg2.connect(**self.db_config) as connection:
+            try:
+                with connection.cursor() as cursor:
+                    for data in data_list:
+                        cursor.execute(query, (data['topic'], data['publication_timestamp'], data['data_id'], data['canonical_url'], data['wigos_station_identifier']))
+                        message_id = cursor.fetchone()[0]
+                        return message_id
+            except Exception as e:
+                # Handle exceptions (log or re-raise)
+                print(f"Error in insert_message_data: {e}")
+                
     def insert_bufr_data(self, message_id, decoded_data):
         # Insert BUFR data into the 'bufr' table
         if message_id is None:
@@ -63,25 +70,26 @@ class DatabaseManager:
             );
         """
 
-        connection = self.connection_pool.getconn()
+        # Create a new connection for each call
+        with psycopg2.connect(**self.db_config) as connection:
+            try:
+                with connection.cursor() as cursor:
+                    for message_data in decoded_data['messages']:
+                        # Reset the values list for each message
+                        values = [message_id]
 
-        try:
-            with connection, connection.cursor() as cursor:
-                for message_data in decoded_data['messages']:
-                    # Reset the values list for each message
-                    values = [message_id]
+                        for key in keys:
+                            value = message_data.get(key.strip(), None)
+                            values.append(value)
 
-                    for key in keys:
-                        value = message_data.get(key.strip(), None)
-                        values.append(value)
+                        # Add the 'raw_data' column with all data in JSON format
+                        keys = required_columns + additional_columns
+                        raw_data = {k: message_data.get(k.strip(), None) for k in keys}
+                        values.append(json.dumps(raw_data))
 
-                    # Add the 'raw_data' column with all data in JSON format
-                    keys = required_columns + additional_columns
-                    raw_data = {k: message_data.get(k.strip(), None) for k in keys}
-                    values.append(json.dumps(raw_data))
+                        sql_query = cursor.mogrify(query, values)
+                        cursor.execute(sql_query)
 
-                    sql_query = cursor.mogrify(query, values)
-                    cursor.execute(sql_query)
-
-        finally:
-            self.connection_pool.putconn(connection)
+            except Exception as e:
+                # Handle exceptions (log or re-raise)
+                print(f"Error in insert_bufr_data: {e}")
